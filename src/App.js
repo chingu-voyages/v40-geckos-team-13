0be32dffc8ee7dfebdfd87e7bba463f93b824e24
TimKlancisar <96549344+TimKlancisar@users.noreply.github.com>
2022-08-08 21:31:54 +0200

    Basic setup
    Created React file structure, added React router, Material UI and Tailwind.

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-slate-200 text-amber-600 font-bold grid justify-center items-center h-24'>Good luck!</div>
+  )
+}
+
+export default App
